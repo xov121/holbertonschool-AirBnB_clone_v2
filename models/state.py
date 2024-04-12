4d68e5ae074a470ab84dbee1c7527bd7a14e6335
xov121 <x.o.v121@gmail.com>
2024-04-12 15:42:23 -0400

    testing hotfix

--- models/state.py
+++ models/state.py
@@ -1,27 +1,23 @@
-#!/usr/bin/python3
-""" State Module for HBNB project
-with a class State that inherits from BaseModel
-and a class attribute name that represents the
- state name with a string (128)"""
 import models
-from models.base_model import BaseModel
-from models.base_model import Base
-from models.city import City
+from os import getenv
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.engine.db_storage import DBStorage
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
+    """State class"""
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
 
-    @property
-    def cities(self):
-        city_list = []
-        for city in list(models.storage.all(City).values()):
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+
+    else:
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            from models.city import City
+            city_list = models.storage.all(City).values()
+            return [city for city in city_list if city.state_id == self.id]
