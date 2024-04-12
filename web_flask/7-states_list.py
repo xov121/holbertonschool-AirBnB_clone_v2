783348b6dd16705ddad77f39e00823d1481dd634
xov121 <x.o.v121@gmail.com>
2024-04-12 16:15:51 -0400

    Fixed task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""Write a script that starts a
+Flask web application"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def hello_8():
+    states_dict = storage.all(State)
+    return render_template('7-states_list.html', states=states_dict.values())
+
+
 @app.teardown_appcontext
-def close_session(exception):
-    """Remove the current SQLAlchemy Session."""
+def s_close(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a HTML page with a list of all State objects."""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
