865aafcf68a69f4f0b6dee95c0ab5c03fbb7c779
xov121 <x.o.v121@gmail.com>
2024-04-12 18:04:39 -0400

    applying various fixes

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,18 +10,22 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of states and cities."""
-    states = storage.all(State).values()
-    # Sorting states by name
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=sorted_states)
+    # Fetch all State objects, sorted by name
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    # For each State, sort its cities by name
+    for state in states:
+        state.cities = sorted(state.cities, key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage on teardown."""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
